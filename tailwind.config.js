8f4422e556cc783d214449b8ebca746b88259fc8
Mahitab Elbehery <mahitab.elbehery@gmail.com>
2024-06-18 17:57:55 +0300

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+export const content = [
+  './index.html',
+  './js/**/*.js',
+  './Css/**/*.css' 
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+     'sans': ['Open Sans', 'sans-serif'],
+     'varela-round': ['Varela Round', 'sans-serif'],
+    },
+    colors: {
+      'custom-green': '#a5c926',
+      'custom-yellow': '#ffc12c',
+      'custom-pink': '#fb416b',
+    },
+    height: {
+      '465': '465px',
+    },
+    screens: {
+      'tab': '992px',
+      'md-mob':'475px',
+      'sm-mob': '375px',
+    },
+  },
+};
+export const plugins = [];
+
+ 
